86f1d4d7c962a8fe28bdffd63991b764e69ad52d
sathiyakumars2308 <sathiyakumars@gmail.com>
2023-03-12 19:55:29 +0530

    Master commit after branchfororgin commit

--- Project/ProjectGit/src/test/java/TestClass.java
+++ Project/ProjectGit/src/test/java/TestClass.java
@@ -60,6 +60,13 @@ public class TestClass {
     }
 
     @Test
+    public void multiplyThree()
+    {
+        NewClass onc=new NewClass(20,21);
+        onc.multiply();
+    }
+
+    @Test
     public void subtractThree()
     {
         NewClass onc=new NewClass(20,21);
