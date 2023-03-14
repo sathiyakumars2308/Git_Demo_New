60d442c11d49441be7fa9c63ac3fafde7604da82
sathiyakumars2308 <sathiyakumars@gmail.com>
2023-03-14 16:28:03 +0530

    Master commit for rebase one

--- Project/ProjectGit/src/test/java/NewClass.java
+++ Project/ProjectGit/src/test/java/NewClass.java
@@ -46,4 +46,10 @@ public class NewClass {
         int c=a/b;
         System.out.println("subtraction of the value is : "+c);
     }
+
+    public void divideTwo()
+    {
+        int c=a/b;
+        System.out.println("subtraction of the value is : "+c);
+    }
 }
